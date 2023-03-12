f1c9ecb7fd7397a1f542eff6eaf8318e792e5086
kevin moreno jaimes <kevinmoreno2106@gmail.com>
2023-03-11 21:15:19 -0500

    .

--- routes/gpt.router.js
+++ routes/gpt.router.js
@@ -8,14 +8,34 @@ router.post('/products', async (req, res) => {
   const data= req.body;
   console.log(data);
   const anwser = await answerService.getKeyWords(data.queryResult.queryText);
-  res.status(200).json({message: anwser});
+  res.status(200).json({
+    fulfillmentMessages: [
+      {
+        text: {
+          text: [
+            anwser
+          ]
+        }
+      }
+    ]
+  });
 });
 
 router.post('/general-info', async (req, res) => {
   const data = req.body;
   console.log(data);
   const anwser = await answerService.testContext(data.queryResult.queryText);
-  res.status(200).json({message: anwser});
+  res.status(200).json({
+    fulfillmentMessages: [
+      {
+        text: {
+          text: [
+            anwser
+          ]
+        }
+      }
+    ]
+  });
 });
 
 module.exports = router;
