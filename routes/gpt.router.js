ad5e824cde5421545faea726cf6f1956436d0c0c
kevin moreno jaimes <kevinmoreno2106@gmail.com>
2023-03-11 21:44:25 -0500

    webhook

--- routes/gpt.router.js
+++ routes/gpt.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AnswerService = require('../services/answer.service');
 const answerService = new AnswerService();
-
+const {WebhookClient} = require('dialogflow-fulfillment');
 
 router.post('/products', async (req, res) => {
   const data= req.body;
@@ -36,6 +36,33 @@ router.post('/general-info', async (req, res) => {
       }
     ]
   });
+});
+
+router.post('/webhook', async (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  async function ConsultaProducto(agent) {
+    const data = req.body;
+    const anwser = await answerService.testContext(data.queryResult.queryText);
+    agent.add(anwser);
+  }
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('Default Fallback Intent', ConsultaProducto);
+  agent.handleRequest(intentMap);
 });
 
 module.exports = router;
